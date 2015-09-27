a2fea2f2a1ce181fe2dece08bd7d3ae54459ae40
Nakia <nakiaaresz@gmail.com>
2015-09-27 15:29:27 +0200

    3. Ending the game

---
+++ Assets/Scripts/ViewModels/Boundary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.ViewModels
+{
+    [System.Serializable]
+    public class Boundary
+    {
+        public float xMin, xMax, zMin, zMax;
+    }
+}
