0fbb0ee8490cab7d68394098fb1fc4bc0186c69d
Nakia <nakiaaresz@gmail.com>
2015-09-27 16:20:17 +0200

    Game is ready

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -22,7 +22,10 @@ public class DestroyByContact : MonoBehaviour {
             return;
         Instantiate(explosion, transform.position, transform.rotation);
         if (other.tag == "Player")
+        {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
         gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,13 +9,32 @@ public class GameController : MonoBehaviour {
     public float startWait;
     public float waveWait;
     public GUIText scoreText;
+    public GUIText restatText;
+    public GUIText gameOverText;
     private int score;
+    private bool gameOver;
+    private bool restart;
 
     void Start()
     {
+        gameOver = false;
+        restart = false;
+        gameOverText.text = "";
+        restatText.text = "";
         score = 0;
         UpdateScore();
         StartCoroutine(SpawnWaves());
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
     }
 
     IEnumerator SpawnWaves()
@@ -23,13 +42,21 @@ public class GameController : MonoBehaviour {
         yield return new WaitForSeconds(startWait);
         while (true)
         {
-            for (int i = 0; i < hazardCount; i++)
+            for (int i = 0; i < hazardCount && !gameOver; i++)
             {
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
             }
+
+            if (gameOver)
+            {
+                restart = true;
+                restatText.text = "Press 'R' for restart";
+                break;
+            }
+
             yield return new WaitForSeconds(waveWait);
         }
     }
@@ -43,5 +70,11 @@ public class GameController : MonoBehaviour {
     {
         score += newScoreValue;
         UpdateScore();
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        gameOverText.text = "Game Over!";
     }
 }
